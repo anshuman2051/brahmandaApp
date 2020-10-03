90684b43ee7adbbd496c4e40e3ef852bec50f847
anshuman2051 <anshumansingh2051@gmail.com>
2020-10-03 13:02:35 +0530

    created basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import {createStore} from 'redux';
+import LandingPage from './containers/landingPage/LandingPage';
+import Asseble from './containers/assemble/Assemble';
+import reducer from './redux/reducer';
+
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">    
+          <Router>
+          <Switch>         
+          <Route
+              path='/assemble'
+              exact={true}
+              render={(props) => (      
+                <Asseble {...props}/>        
+              )}
+            />
+            <Route
+              path='/'
+              exact={true}
+              render={(props) => (      
+                <LandingPage {...props}/>        
+              )}
+            />            
+          </Switch>  
+        </Router>
+      </div>
+    </Provider>    
   );
 }
 
---
+++ src/containers/assemble/Assemble.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Assemble extends React.Component{
+    render(){
+        return(
+            <div>
+                Assembly
+            </div>
+        );
+    }
+}
+
+export default Assemble;
