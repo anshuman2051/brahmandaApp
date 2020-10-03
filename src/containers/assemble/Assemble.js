f706d309d7cba10b2bc4f7084b352be05e3a81c7
anshuman2051 <anshumansingh2051@gmail.com>
2020-10-03 14:00:03 +0530

    designing assembly page

--- src/containers/assemble/Assemble.js
+++ src/containers/assemble/Assemble.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import classes from './Assemble.module.css';
+
+const EnginePart =(props)=>{
+    return(
+        <div className={classes.enginePart}>
+            <img 
+                alt="imagePic" 
+                src="https://wiki.kerbalspaceprogram.com/images/c/c1/Oscar-B_FT.png"
+                />
+            <span>{props.name}</span>
+        </div>
+    );    
+}
 
 class Assemble extends React.Component{
     render(){
         return(
-            <div>
-                Assembly
+            <div className={classes.container}>
+                <div className={classes.sideNav}>
+                    <div className={classes.parts}>
+
+                    </div>
+                    <div className={classes.engines}>
+                        <EnginePart name="abc"/>
+                        <EnginePart name="def"/>
+                    </div>
+                    <div className={classes.fuelTanks}>
+
+                    </div>                    
+                </div>
             </div>
         );
     }
