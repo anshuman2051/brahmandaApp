d34ecd85b99e3e5ba0ab8b8155542c3ac1e84c8f
Sahil Mishra <sahilmishra0012@outlook.com>
2020-10-03 21:09:30 +0530

    Added Landing Page

--- src/containers/landingPage/LandingPage.js
+++ src/containers/landingPage/LandingPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import SideBar from "./sidebar";
+import './LandingPage.css';
 
-class LandingPage extends React.Component{
-    render(){
-        return(
-            <div>
-                helo world
+class LandingPage extends React.Component {
+    render() {
+        return ( 
+            <div id="App" className = 'Login-component'>
+                <div align="right"><SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} /></div>
+            <div id="page-wrap">
+            <h1 className = 'body-font'> Brahmanda </h1>
             </div>
+          </div>
+
         );
     }
 }
