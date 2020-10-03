1bc51a001bce1be7b2df10241816f1a255e82db4
anshuman2051 <anshumansingh2051@gmail.com>
2020-10-03 16:40:15 +0530

    changes in design of assembly

--- src/containers/assemble/Assemble.js
+++ src/containers/assemble/Assemble.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import classes from './Assemble.module.css';
 
-const EnginePart =(props)=>{
-    return(
-        <div className={classes.enginePart}>
+const PartDesc = props=>{
+    return (
+        <div className={classes.partDesc}>
+            <div>
+                <div>{props.name}</div>
+                <p>
+                    This is a sample description...
+                </p>
+            </div>            
             <img 
-                alt="imagePic" 
-                src="https://wiki.kerbalspaceprogram.com/images/c/c1/Oscar-B_FT.png"
-                />
-            <span>{props.name}</span>
+            alt="imagePic" 
+            src="https://wiki.kerbalspaceprogram.com/images/c/c1/Oscar-B_FT.png"
+            />                        
+        </div>
+    );
+}
+
+const DescriptionBox =(props)=>{
+    return(
+        <div className={classes.descriptionBox}>    
+            <PartDesc name="desc1"/>
+            <PartDesc name="desc1"/>
+            <PartDesc name="desc1"/>           
         </div>
     );    
+}
+
+
+const Part = props=>{
+    return(
+        <div className={classes.partName}>
+            Nose
+        </div>
+    );
 }
 
 class Assemble extends React.Component{
@@ -19,16 +43,17 @@ class Assemble extends React.Component{
             <div className={classes.container}>
                 <div className={classes.sideNav}>
                     <div className={classes.parts}>
-
-                    </div>
-                    <div className={classes.engines}>
-                        <EnginePart name="abc"/>
-                        <EnginePart name="def"/>
-                    </div>
-                    <div className={classes.fuelTanks}>
-
+                        <Part/>
+                        <Part/>
+                        <Part/>
+                        <Part/>
                     </div>                    
                 </div>
+                <div>
+                    <div className={classes.desc}>
+                            <DescriptionBox name="abc"/>                        
+                    </div>                    
+                </div>                
             </div>
         );
     }
