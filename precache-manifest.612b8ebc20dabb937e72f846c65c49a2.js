1ff38c46055d80bae6deb30319d9879bbbd60694
Sahil Mishra <sahilmishra0012@outlook.com>
2020-10-04 22:37:15 +0530

    Build files for GitHub pages

---
+++ precache-manifest.612b8ebc20dabb937e72f846c65c49a2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c15f058da391970bcae4e8eab69866b",
+    "url": "/brahmandaApp/index.html"
+  },
+  {
+    "revision": "d82b41060018ab192df4",
+    "url": "/brahmandaApp/static/css/main.107840bf.chunk.css"
+  },
+  {
+    "revision": "607126c9bb4fcfa195ae",
+    "url": "/brahmandaApp/static/js/2.6f5651d9.chunk.js"
+  },
+  {
+    "revision": "89a6b555af4184298a5af61b72cf43aa",
+    "url": "/brahmandaApp/static/js/2.6f5651d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82b41060018ab192df4",
+    "url": "/brahmandaApp/static/js/main.31a5cc02.chunk.js"
+  },
+  {
+    "revision": "e15e2bd16d5b7aff819f",
+    "url": "/brahmandaApp/static/js/runtime-main.e94ac064.js"
+  },
+  {
+    "revision": "82987827f2526914f03f1488ebda55a2",
+    "url": "/brahmandaApp/static/media/brave.82987827.ttf"
+  },
+  {
+    "revision": "603a56d0d52fe6bbfd13b08a1195b7e8",
+    "url": "/brahmandaApp/static/media/chops.603a56d0.ttf"
+  },
+  {
+    "revision": "75a70cc90632f5164141cabee4c6e313",
+    "url": "/brahmandaApp/static/media/earth.75a70cc9.png"
+  },
+  {
+    "revision": "15011d8f291abf717524e0c0b65046cc",
+    "url": "/brahmandaApp/static/media/engine1.15011d8f.png"
+  },
+  {
+    "revision": "2995c17b0f72c03a9e3dbc8ad6940b49",
+    "url": "/brahmandaApp/static/media/engine2.2995c17b.png"
+  },
+  {
+    "revision": "eda5215d6e6296e63b7b6f0439948ed5",
+    "url": "/brahmandaApp/static/media/fuel_first.eda5215d.png"
+  },
+  {
+    "revision": "7396c7fb52092b342d3c766581efd8c5",
+    "url": "/brahmandaApp/static/media/fuel_sec_big.7396c7fb.png"
+  },
+  {
+    "revision": "c122d6228bfe01ceea4724dd8a9c0cf7",
+    "url": "/brahmandaApp/static/media/fuel_sec_small.c122d622.png"
+  },
+  {
+    "revision": "9206af3310dbc426d9f0deb1c3ce920f",
+    "url": "/brahmandaApp/static/media/jupiter.9206af33.png"
+  },
+  {
+    "revision": "0688b72734094099988e3bfd9729db57",
+    "url": "/brahmandaApp/static/media/landing_background.0688b727.jpg"
+  },
+  {
+    "revision": "39b7cf9ce56f87307ffaa57237410d16",
+    "url": "/brahmandaApp/static/media/mars.39b7cf9c.png"
+  },
+  {
+    "revision": "249cdd2cf02b830c3b8d5db6b417f6de",
+    "url": "/brahmandaApp/static/media/mercury.249cdd2c.png"
+  },
+  {
+    "revision": "006c61b35e5d1a6e7c4c907c9836fb83",
+    "url": "/brahmandaApp/static/media/neptune.006c61b3.png"
+  },
+  {
+    "revision": "7d0670530c9e6a305fa283d586a6ec91",
+    "url": "/brahmandaApp/static/media/paladins.7d067053.otf"
+  },
+  {
+    "revision": "b7f531aab053610f78b7f7178483ecfa",
+    "url": "/brahmandaApp/static/media/pointed_nose.b7f531aa.png"
+  },
+  {
+    "revision": "18c2ab60a8c6c0545c197618758dd69c",
+    "url": "/brahmandaApp/static/media/rocket.18c2ab60.png"
+  },
+  {
+    "revision": "44e7284431a8bb71df6527de73e2d427",
+    "url": "/brahmandaApp/static/media/rocket.44e72844.jpg"
+  },
+  {
+    "revision": "f0aa7cf3f20a288125c797372c58b241",
+    "url": "/brahmandaApp/static/media/round_nose.f0aa7cf3.png"
+  },
+  {
+    "revision": "b0b984318d9fef50cfcef2da6ec0a4a0",
+    "url": "/brahmandaApp/static/media/saturn.b0b98431.png"
+  },
+  {
+    "revision": "f89062b37f4180db08a82f87b6970255",
+    "url": "/brahmandaApp/static/media/sun.f89062b3.png"
+  },
+  {
+    "revision": "257075c1c56981fae41417288391660f",
+    "url": "/brahmandaApp/static/media/uranus.257075c1.png"
+  },
+  {
+    "revision": "86576646d1bd992acf07c26a764d023d",
+    "url": "/brahmandaApp/static/media/venus.86576646.png"
+  }
+]);
