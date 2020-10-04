f42912cc019426617eea370ba07fc59ad7961ac2
Sahil Mishra <sahilmishra0012@outlook.com>
2020-10-04 11:57:26 +0530

    Updated style and layout

--- src/containers/assemble/Assemble.js
+++ src/containers/assemble/Assemble.js
@@ -4,177 +4,204 @@ import DescriptionBox from './DescriptionBox';
 import Rocket from './Rocket';
 
 
-class Assemble extends React.Component{
+class Assemble extends React.Component {
     state = {
-        data : [
+        data: [
             {
-                name : "Nose",
-                parts : [
+                name: "Nose",
+                parts: [
                     {
-                        name : "nose 1",
-                        desc : "adfssdfsdfsfsd",
-                        image : require('../../res/fuel.png'),
-                        specs : [
-                            ["spec1" , "value"],
-                            ["key2" , "value2"]
+                        name: "nose 1",
+                        desc: "adfssdfsdfsfsd",
+                        image: require('../../res/fuel1.png'),
+                        specs: [
+                            ["spec1", "value"],
+                            ["key2", "value2"]
                         ]
                     },
                     {
-                        name : "nose 2",
-                        desc : "adfssdfsdfsfsd",
-                        image : require('../../res/fuel.png'),
-                        specs : [
-                            ["spec1" , "value"],
-                            ["key2" , "value2"]
+                        name: "nose 2",
+                        desc: "adfssdfsdfsfsd",
+                        image: require('../../res/fuel1.png'),
+                        specs: [
+                            ["spec1", "value"],
+                            ["key2", "value2"]
                         ]
                     }
                 ]
             },
             {
-                name : "Fuel Tank",
-                parts : [
+                name: "Fuel Tank",
+                parts: [
                     {
-                        name : "Oscar-B Fuel Tank",
-                        desc : "The Oscar-B Fuel Tank provides fuel to attached liquid fuel engines.",
-                        image : "https://wiki.kerbalspaceprogram.com/images/c/c1/Oscar-B_FT.png",
-                        specs : [
-                            ["mass" , "0.23t"],
-                            ["drag" , "0.15-0.2"]                            
+                        name: "Fuel Tank 1",
+                        desc: "The Oscar-B Fuel Tank provides fuel to attached liquid fuel engines.",
+                        image: require("../../res/fuel1.png"),
+                        specs: [
+                            ["mass", "0.23t"],
+                            ["drag", "0.15-0.2"]
+                        ]
+                    },
+                    {
+                        name: "Fuel Tank 2",
+                        desc: "The Oscar-B Fuel Tank provides fuel to attached liquid fuel engines.",
+                        image: require("../../res/fuel2.png"),
+                        specs: [
+                            ["mass", "0.23t"],
+                            ["drag", "0.15-0.2"]
+                        ]
+                    },
+                    {
+                        name: "Fuel Tank 3",
+                        desc: "The Oscar-B Fuel Tank provides fuel to attached liquid fuel engines.",
+                        image: require("../../res/fuel3.png"),
+                        specs: [
+                            ["mass", "0.23t"],
+                            ["drag", "0.15-0.2"]
                         ]
                     }
                 ]
             },
             {
-                name : "Engine Type",
-                parts : [
+                name: "Engine Type",
+                parts: [
                     {
-                        name : "nose 1",
-                        desc : "adfssdfsdfsfsd",
-                        image : "https://wiki.kerbalspaceprogram.com/images/c/c1/Oscar-B_FT.png",
-                        specs : [
-                            ["spec1" , "value"],
-                            ["key2" , "value2"]
+                        name: "Engine 1",
+                        desc: "adfssdfsdfsfsd",
+                        image: require("../../res/engine1.png"),
+                        specs: [
+                            ["spec1", "value"],
+                            ["key2", "value2"]
+                        ]
+                    },
+                    {
+                        name: "Engine 2",
+                        desc: "adfssdfsdfsfsd",
+                        image: require("../../res/engine2.png"),
+                        specs: [
+                            ["spec1", "value"],
+                            ["key2", "value2"]
                         ]
                     }
                 ]
             },
             {
-                name : "Nozzle",
-                parts : [
+                name: "Nozzle",
+                parts: [
                     {
-                        name : "nose 1",
-                        desc : "adfssdfsdfsfsd",
-                        image : "https://wiki.kerbalspaceprogram.com/images/c/c1/Oscar-B_FT.png",
-                        specs : [
-                            ["spec1" , "value"],
-                            ["key2" , "value2"]
+                        name: "nose 1",
+                        desc: "adfssdfsdfsfsd",
+                        image: "https://wiki.kerbalspaceprogram.com/images/c/c1/Oscar-B_FT.png",
+                        specs: [
+                            ["spec1", "value"],
+                            ["key2", "value2"]
                         ]
                     }
                 ]
             }
         ],
-        selectedIndex : -1,
-        rocketContents : []
+        selectedIndex: -1,
+        rocketContents: []
     };
 
-    handleClick = (evt)=>{    
-        evt.persist()        
-        console.log(evt.target?.id);    
-        this.setState(prev =>{                        
-            return {                
+    handleClick = (evt) => {
+        evt.persist()
+        console.log(evt.target?.id);
+        this.setState(prev => {
+            return {
                 selectedIndex: prev?.selectedIndex === evt.target?.id ? -1 : evt.target?.id
             };
-    });
+        });
     }
 
-    addPart = ( part ) =>{                
-        this.setState( prev=>{
+    addPart = (part) => {
+        this.setState(prev => {
             let partsArr = [...prev.rocketContents];
 
-            switch( prev.selectedIndex){
-                case 0 :
+            switch (prev.selectedIndex) {
+                case 0:
                     partsArr = [
                         part,
                         ...partsArr
                     ]
                     break;
-                case 2 : 
-                    partsArr = [                     
+                case 2:
+                    partsArr = [
                         ...partsArr,
                         part,
                     ]
                     break;
                 default:
-                    if( partsArr?.length === 0){
+                    if (partsArr?.length === 0) {
                         partsArr = [part];
                     }
-                    else if( partsArr?.length === 1){
-                        partsArr = [                     
+                    else if (partsArr?.length === 1) {
+                        partsArr = [
                             ...partsArr,
                             part,
                         ]
                     }
-                    else{
+                    else {
                         partsArr = [
-                            ...partsArr.slice(0,partsArr?.length - 1),
+                            ...partsArr.slice(0, partsArr?.length - 1),
                             part,
                             partsArr[partsArr?.length - 1]
                         ]
-                    }    
+                    }
             }
             return {
-                rocketContents : partsArr
+                rocketContents: partsArr
             };
         })
     }
 
-    removePart = (key)=>{        
-        this.setState( prev=>{
-            const partsArr = prev.rocketContents;                
+    removePart = (key) => {
+        this.setState(prev => {
+            const partsArr = prev.rocketContents;
             partsArr.splice(key, 1);
             console.log(partsArr, key);
             return {
-                rocketContents : partsArr
+                rocketContents: partsArr
             }
-        })        
+        })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className={classes.container}>
                 <div className={classes.sideNav}>
                     <div className={classes.parts}>
                         {
-                            this.state?.data?.map( (data, key)=>{
+                            this.state?.data?.map((data, key) => {
                                 return (
                                     <div key={key} className={classes.partName} id={key} onClick={this.handleClick} >
                                         {data?.name}
-                                    </div>                                    
+                                    </div>
                                 )
                             })
-                        }                        
-                    </div>                    
+                        }
+                    </div>
                 </div>
-                <div style={{width:"100%"}} className={classes.contentBody}> 
+                <div style={{ width: "100%" }} className={classes.contentBody}>
                     {
-                        this.state.selectedIndex !== -1?
-                        <div className={classes.desc}>
-                            <DescriptionBox name="abc" partsData={this.state.data[this.state.selectedIndex]} addPart={this.addPart}/>                        
-                        </div>                    
-                        :
-                        null
+                        this.state.selectedIndex !== -1 ?
+                            <div className={classes.desc}>
+                                <DescriptionBox name="abc" partsData={this.state.data[this.state.selectedIndex]} addPart={this.addPart} />
+                            </div>
+                            :
+                            null
                     }
-                </div>     
+                </div>
                 <div className={classes.rocketContainer}>
-                    <Rocket data={this.state?.rocketContents} removePart={this.removePart}/>
-                </div>                    
-                                   
-                <center 
-                    className={classes.launchBtn + (this.state.rocketContents?.length ? "" :  (" ab "+ classes.disabled))}
-                    onClick = { (this.state.rocketContents?.length ?  ()=>this.props.history.push("/launch"): null) }
+                    <Rocket data={this.state?.rocketContents} removePart={this.removePart} />
+                </div>
+
+                <center
+                    className={classes.launchBtn + (this.state.rocketContents?.length ? "" : (" ab " + classes.disabled))}
+                    onClick={(this.state.rocketContents?.length ? () => this.props.history.push("/launch") : null)}
                 >
                     Launch !
-                </center>                                
+                </center>
             </div>
         );
     }
